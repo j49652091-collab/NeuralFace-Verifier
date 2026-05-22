5af055df7a9aeeab34d16d61c0ea19b53d8977a4
j49652091-collab <j49652091@gmail.com>
2026-05-22 22:44:48 +0300

    Integrate AI image classification using transformers

--- detector.py
+++ detector.py
@@ -1,15 +1,50 @@
 from PIL import Image
-import random
+import torch
+from transformers import pipeline
+
+classifier=pipeline(
+"image-classification",
+model="umm-maybe/AI-image-detector"
+)
 
 def detect_image(uploaded_image):
 
-    image=Image.open(uploaded_image)
+    image=Image.open(
+    uploaded_image
+    )
 
-    score=random.uniform(0,100)
+    result=classifier(image)
 
-    if score>50:
-        result="AI Generated"
+    ai_score=0
+    real_score=0
+
+    for item in result:
+
+        label=item["label"]
+        score=item["score"]
+
+        if "AI" in label:
+            ai_score=score
+
+        else:
+            real_score=score
+
+    if ai_score>real_score:
+
+        status="AI Generated"
+
+        confidence=round(
+        ai_score*100,
+        2
+        )
+
     else:
-        result="Real Human"
 
-    return result,score,image
+        status="Real Human"
+
+        confidence=round(
+        real_score*100,
+        2
+        )
+
+    return status,confidence,image
