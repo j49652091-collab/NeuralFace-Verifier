1185e16b44889fadb2dfac7a407bb7baae74da0b
j49652091-collab <j49652091@gmail.com>
2026-05-23 11:11:41 +0300

    Update login.py

--- login.py
+++ login.py
@@ -3,9 +3,15 @@ import json
 def login(username,password):
 
     with open("users.json","r") as f:
+
         user=json.load(f)
 
-    if username==user["username"] and password==user["password"]:
+    if(
+        username==user["username"]
+        and
+        password==user["password"]
+    ):
+
         return True
 
     return False
