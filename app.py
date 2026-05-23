3ec8ee3e3ec4797fccdee08ef1334cd6b7fd21a0
j49652091-collab <j49652091@gmail.com>
2026-05-23 10:51:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
-import subprocess
-import sys
-
-# خدعة برمجية لحذف النسخة المسببة للمشكلة وإجبار السيرفر على نسخة السحاب
-try:
-    subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "-y", "opencv-python", "opencv-contrib-python"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "opencv-python-headless"])
-except Exception as e:
-    pass
-
-# الآن تبدأ استدعاءات الكود الخاص بك بشكل طبيعي تماماً
-import streamlit as st
-import tensorflow as tf
-# ... باقي الكود الخاص بك
-
-
-
-
-
 
 
 
